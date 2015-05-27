bd278927ebd30fd3df7950e5d9d0b11d7e53f1ea
gohai <gottfried.haider@gmail.com>
2015-05-27 07:33:47 +0200

    Remove previous Processing versions from /usr/local/lib

--- BuildRaspbianVc4.py
+++ BuildRaspbianVc4.py
@@ -413,7 +413,8 @@ def buildExtraProcessing():
 	os.chdir("/usr/local/src/processing/build")
 	# we could build Processing with a more recent Java version
 	subprocess.check_call("ant linux-build", shell=True)
-	subprocess.check_call("rm -rf /usr/local/lib/processing" + PROCESSING_VERSION, shell=True)
+	# this also removes previous versions
+	subprocess.check_call("rm -rf /usr/local/lib/processing*", shell=True)
 	subprocess.check_call("mv linux/work /usr/local/lib/processing" + PROCESSING_VERSION, shell=True)
 	subprocess.check_call("chown root:root -R /usr/local/lib/processing" + PROCESSING_VERSION, shell=True)
 	subprocess.check_call("ln -sf /usr/local/lib/processing" + PROCESSING_VERSION + "/processing /usr/local/bin/processing", shell=True)
