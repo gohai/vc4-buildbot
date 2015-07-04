da340185a52fca369fe9bc848dd63bbb90169a32
gohai <gottfried.haider@gmail.com>
2015-07-04 12:44:12 +0200

    f7ac3b7d3d69e15e63910d2a5492ba487ddf4abb seem to have done the trick, disable debugging again

--- BuildRaspbianVc4.py
+++ BuildRaspbianVc4.py
@@ -14,10 +14,10 @@ import subprocess
 import re
 import json
 
-LINUX_GIT_REPO_2708 = "https://github.com/gohai/linux.git"
-LINUX_GIT_BRANCH_2708 = "vc4-kms-v3d-rpi2-debug"
-LINUX_GIT_REPO_2709 = "https://github.com/gohai/linux.git"
-LINUX_GIT_BRANCH_2709 = "vc4-kms-v3d-rpi2-debug"
+LINUX_GIT_REPO_2708 = "https://github.com/anholt/linux.git"
+LINUX_GIT_BRANCH_2708 = "vc4-kms-v3d-rpi2"
+LINUX_GIT_REPO_2709 = "https://github.com/anholt/linux.git"
+LINUX_GIT_BRANCH_2709 = "vc4-kms-v3d-rpi2"
 MESA_GIT_REPO = "git://anongit.freedesktop.org/mesa/mesa"
 MESA_GIT_BRANCH = "master"
 PROCESSING_GIT_REPO = "https://github.com/gohai/processing.git"
