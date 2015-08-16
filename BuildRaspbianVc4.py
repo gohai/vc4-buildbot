c904b6f0cc0add6bfda97454f21778eec3bd2808
gohai <gottfried.haider@gmail.com>
2015-08-16 15:09:56 +0200

    Make the chmod for LEDs more general

--- BuildRaspbianVc4.py
+++ BuildRaspbianVc4.py
@@ -128,9 +128,9 @@ def updateRcLocalForLeds():
 	# LEDs can only be controlled by the root user by default
 	# couldn't get a udev rule for this to work
 	rclocal = file_get_contents("/etc/rc.local")
-	match = re.findall(r'^chmod -R a+rw /sys/devices/platform/soc/soc:leds/leds$', rclocal, re.MULTILINE)
+	match = re.findall(r'^chmod -R a\+rw /sys/class/leds/\*$', rclocal, re.MULTILINE)
 	if 0 == len(match):
-		rclocal = re.sub('exit 0', '# allow regular users to control the leds\nchmod -R a+rw /sys/devices/platform/soc/soc:leds/leds\n\nexit 0', rclocal)
+		rclocal = re.sub('exit 0', '# allow regular users to control the leds\nchmod -R a+rw /sys/class/leds/*\n\nexit 0', rclocal)
 		file_put_contents("/etc/rc.local", rclocal)
 
 def getGitInfo():
